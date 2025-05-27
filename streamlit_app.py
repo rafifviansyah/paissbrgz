eaa75f72089c194206d70d591e0367948035eaf7
rafifviansyah <rafifviansyah603@gmail.com>
2025-05-27 13:39:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("paissbgrzmph")
+st.title("paissbrigezmph")
 st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/")
 st.image("IMG-20250201-WA0030.jpg", width=200)
 
